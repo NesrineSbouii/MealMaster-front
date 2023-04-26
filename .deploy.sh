3dc6c762aceee8f47370c743035519def189e52c
Nesrine SBOUI <nesrinesboui@Nesrines-MacBook-Pro.local>
2023-04-26 11:56:08 +0200

    fix deploy error node version

--- .deploy.sh
+++ .deploy.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-echo "Node.js Version: 16.20.0" >> /dev/null 2>&1
+
+# Print out the Node.js version for debugging purposes
+echo "Node.js Version: $(node -v)"
+
+# Navigate to the deployment directory
 cd "$DEPLOYMENT_TARGET"
-# Add your deployment commands here
+
+# Set the Node.js version
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm use 16.20.0
+
+# Install dependencies
+npm install
+
+# Build your Angular application
+ng build --prod
+
+# Rename the "dist" directory to "wwwroot"
+mv dist wwwroot
