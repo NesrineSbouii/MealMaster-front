82df6b88d228aaf524fb8f84cd448bfa46fe18e5
Nesrine SBOUI <nesrinesboui@Nesrines-MacBook-Pro.local>
2023-05-23 17:29:35 +0200

    update login service

--- src/app/service/auth/login.service.ts
+++ src/app/service/auth/login.service.ts
@@ -1,16 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-
+import { Router } from '@angular/router';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
-  baseUrl = environment.baseUrl
-  constructor(private http: HttpClient) { }
+  baseUrl = environment.baseUrl;
+  constructor(private http: HttpClient, private router: Router) {}
 
   login(username: string, password: string) {
     // Send a POST request to the Django backend with the username and password
-    return this.http.post(this.baseUrl + '/login/', { username, password });
+    return this.http
+      .post(this.baseUrl + '/login/', { username, password })
+      .subscribe(
+        (response: any) => {
+          if (response.token) {
+            // Redirect to "/dashboard" if the response contains a token
+            this.router.navigate(['/dashboard']);
+          }
+        },
+        (error: any) => {
+          // Handle error response
+        }
+      );
   }
 }
