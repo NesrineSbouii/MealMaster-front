0d64b229f0bd7c1b526505388c0fba50f15cf14e
Nesrine SBOUI <nesrinesboui@Nesrines-MacBook-Pro.local>
2023-05-24 16:14:59 +0200

    fix corss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS, HttpXsrfTokenExtractor, HttpRequest, HttpInterceptor, HttpHandler, HttpEvent, HttpClientXsrfModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -10,41 +10,28 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-export class CustomXsrfTokenExtractor implements HttpXsrfTokenExtractor {
-  getToken(): string | null {
-    // Implement your logic to get the token
-    return 'my-xsrf-token';
-  }
-}
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     LoginComponent,
     RegisterComponent,
-   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
-   
-   
+    ReactiveFormsModule,
   ],
-  providers: [ImageService, {
-    provide: HTTP_INTERCEPTORS,
-    useClass: CustomInterceptor,
-    multi: true
-  },
-  {
-    provide: HttpXsrfTokenExtractor,
-    useClass: CustomXsrfTokenExtractor
-  }],
-  bootstrap: [AppComponent]
+  providers: [
+    ImageService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CustomInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
-
-
-
+export class AppModule {}
