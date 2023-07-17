bc6fc5c9eafd1d75ced1e8e0a8bce3999de031ff
Nesrine SBOUI <nesrinesboui@Nesrines-MacBook-Pro.local>
2023-07-17 15:00:51 +0200

    add auth and register service

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,31 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { RegisterService } from 'src/app/service/register/register.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-
   registrationForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private registerService: RegisterService
+  ) {}
 
   ngOnInit() {
-    this.registrationForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.required]
-    }, { validator: this.passwordMatchValidator });
+    this.registrationForm = this.formBuilder.group(
+      {
+        username: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+      },
+      { validator: this.passwordMatchValidator }
+    );
   }
 
   onSubmit() {
     if (this.registrationForm.valid) {
-      // Submit the form data
-      console.log(this.registrationForm.value);
+      this.registerService.userRegister({
+        username: this.username,
+        email: this.email,
+        password: this.password,
+      });
     }
+  }
+  get username(): string {
+    return this.registrationForm.get('username')?.value;
+  }
+  get email(): string {
+    return this.registrationForm.get('email')?.value;
+  }
+  get password(): string {
+    return this.registrationForm.get('password')?.value;
   }
 
   onCancel() {
@@ -35,10 +58,13 @@ export class RegisterComponent implements OnInit {
   passwordMatchValidator(control: FormControl): { [key: string]: boolean } {
     const password = control.get('password');
     const confirmPassword = control.get('confirmPassword');
-    if (password && confirmPassword && password.value !== confirmPassword.value) {
-      return { 'passwordMismatch': true };
+    if (
+      password &&
+      confirmPassword &&
+      password.value !== confirmPassword.value
+    ) {
+      return { passwordMismatch: true };
     }
     return {};
   }
-
 }
